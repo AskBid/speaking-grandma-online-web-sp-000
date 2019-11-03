8b2885c6e10e5c9891c1daee6b2ed0f0f5f1e140
sergio marelli <sergio.mrll@gmail.com>
2019-11-03 10:53:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ def speak_to_grandma(heard)
   if heard == 'I LOVE YOU GRANDMA!'
     'I LOVE YOU TOO PUMPKIN!'
   end
-  if heard != phrase.upcase()
+  if heard != heard.upcase()
     return "HUH?! SPEAK UP, SONNY!"
   else
     return "NO, NOT SINCE 1938!"
